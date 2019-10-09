ab7dca28db1285c3ec23f03e2483b5add18e34de
Aditya Nugraha <anugraha@redhat.com>
2019-10-09 14:32:49 +0700

    jdg

---
+++ fuse-jdg/src/main/java/org/jboss/fis2/demo/rest/Application.java
@@ -0,0 +1,50 @@
+/**
+ *  Copyright 2005-2016 Red Hat, Inc.
+ *
+ *  Red Hat licenses this file to you under the Apache License, version
+ *  2.0 (the "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ *  implied.  See the License for the specific language governing
+ *  permissions and limitations under the License.
+ */
+package org.jboss.fis2.demo.rest;
+
+import org.apache.cxf.transport.servlet.CXFServlet;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ImportResource;
+
+import infinispan.org.jboss.logging.BasicLogger;
+
+/**
+ * The Spring-boot main class.
+ */
+@SpringBootApplication
+@ImportResource({"classpath:spring/amqp-context.xml", "classpath:spring/json.xml", "classpath:spring/camel-context.xml"})
+public class Application {
+
+    public static void main(String[] args) {
+    	
+    	org.infinispan.commons.logging.BasicLogFactory lf = new org.infinispan.commons.logging.BasicLogFactory();
+    	BasicLogger bl = lf.getLog(Application.class);
+    	
+        SpringApplication.run(Application.class, args);
+    }
+    
+    @Bean
+    ServletRegistrationBean servletRegistrationBeanCXF() {
+        ServletRegistrationBean servlet = new ServletRegistrationBean(
+            new CXFServlet(), "/cxf/*");
+        servlet.setName("CXFServlet");
+        return servlet;
+    }
+
+}
